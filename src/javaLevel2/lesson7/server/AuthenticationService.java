15641143faae127ea479865369bc648c02f1ffa6
gismo8992 <gismo8992@mail.ru>
2021-04-19 00:56:13 +0300

    Homework2.Level3.

--- src/javaLevel2/lesson7/server/AuthenticationService.java
+++ src/javaLevel2/lesson7/server/AuthenticationService.java
@@ -1,18 +1,28 @@
 package javaLevel2.lesson7.server;
 
-import java.util.Optional;
-import java.util.Set;
-
 public class AuthenticationService {
 
-    private static final Set<Entry> entries = Set.of(
+    private final UserRepository repository;
+
+    public AuthenticationService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public User findUser(String login, String password) {
+        return repository.findByLoginAndPassword(login, password).orElseThrow(
+                () -> new RuntimeException("No user found with login " + login)
+        );
+    }
+
+
+    /*private static final Set<Entry> entries = Set.of(
             new Entry("User1", "l1", "p1"),
             new Entry("User2", "l2", "p2"),
             new Entry("User3", "l3", "p3")
     );
 
     public Optional<Entry> findEntryByCredentials(String login, String password) {
-        /*
+        *//*
         Iterator<Entry> iterator = entries.iterator();
         while (iterator.hasNext()) {
             Entry next = iterator.next();
@@ -21,13 +31,13 @@ public class AuthenticationService {
             }
         }
         return Optional.empty();
-        */
+        *//*
         return entries.stream()
                 .filter(entry -> entry.getLogin().equals(login) && entry.getPassword().equals(password))
                 .findFirst();
-    }
+    }*/
 
-    public static class Entry{
+    /*public static class Entry{
         private String name;
         private String login;
         private String password;
@@ -49,5 +59,5 @@ public class AuthenticationService {
         public String getPassword() {
             return password;
         }
-    }
+    }*/
 }
